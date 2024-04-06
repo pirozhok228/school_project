8a11f59323d5530945a84395f23b2a9493f338c5
pirozhok228 <vadimpirogov505@gmail.com>
2024-04-06 15:19:24 +0300

    Верстка главной страницы

--- my-app/app/components/Header/Header.jsx
+++ my-app/app/components/Header/Header.jsx
@@ -1,19 +1,20 @@
 import Styles from './Header.module.css'
+import Link from 'next/link'
 
 export const Header = () => {
     return (
         <header className={Styles['header']}>
             <div className={Styles['header-container']}>
-                <div className={Styles['logo']}>
-                    <img src="/images/gerb.png" alt="Что-то пошло не так..." height="84"/>
-                    <h1 className={Styles['logo-title']}>Медиацентр<br />Гимназии №97<br />г. Ельца</h1>
-                </div>
+                <Link href='http://127.0.0.1:3000/'>
+                    <div className={Styles['logo']}>
+                        <img src="/images/gerb.png" alt="Что-то пошло не так..." height="84" />
+                        <h1 className={Styles['logo-title']}>Медиацентр<br />Гимназии №97<br />г. Ельца</h1>
+                    </div>
+                </Link>
                 <nav>
                     <ul className={Styles['ul-links']}>
-                        <a className={Styles['link']} href="http://127.0.0.1:5000/">Главная</a>
-                        <a className={Styles['link']} href="http://127.0.0.1:5000/news">Новости</a>
-                        <a className={Styles['link']} href="http://127.0.0.1:5000/declorations">Объявления</a>
-                        <a className={Styles['link']} href="http://127.0.0.1:5000/about_us">О нас</a>
+                        <Link className={Styles['link']} href="http://127.0.0.1:3000/news">Новости</Link>
+                        <Link className={Styles['link']} href="http://127.0.0.1:3000/declorations">Объявления</Link>
                     </ul>
                 </nav>
             </div>
--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,12 +1,57 @@
+import Styles from './page.module.css'
+import { Slider } from './components/Slider/Slider'
+
 export default function Home() {
     return (
-        <main class="main">
-        <p>Самые свежие новости школьной жизни</p>
-            <div class="grid-img">
-                <img src="/images/1.jpg" alt="" width="500" />
-                <img src="/images/2.jpg" alt="" width="500" />
-                <img src="/images/3.jpg" alt="" width="500" />
-                <img src="/images/школа.jpg" alt="" width="500" />
+        <main className={Styles["main"]}>
+            <div className={Styles["titles"]}>
+                <h1 className={Styles["title"]}>Школьный медиацентр МБОУ Гиманзии №97 г.Ельца</h1>
+                <h2 className={Styles["title"]}>Самые свежие новости из школьной жизни</h2>
+            </div>
+
+            <div className={Styles["main-container"]}>
+                <div className={Styles["main-div"]}>
+                    <img src='/images/школа.jpg' className={Styles["main-img"]} />
+                    <div className={Styles["text-container"]}>
+                        <h3 className={Styles["main-h3"]}>Немного о нашей школе</h3>
+                        <p className={Styles["main-p"]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore
+                            et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                            deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+                <div className={Styles["main-div"]}>
+                    <div className={Styles["text-container"]}>
+                        <h3 className={Styles["main-h3"]}>Немного о нашем проекте</h3>
+                        <p className={Styles["main-p"]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore
+                            et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                            deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                    <img src='/images/room.png' className={Styles["main-img"]} />
+                </div>
+                <div className={Styles["main-div"]}>
+                    <img src='/images/2.jpg' className={Styles["main-img"]} />
+                    <div className={Styles["text-container"]}>
+                        <h3 className={Styles["main-h3"]}>И ещё немного о нашей команде</h3>
+                        <p className={Styles["main-p"]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore
+                            et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                            deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
             </div>
         </main>
     )
